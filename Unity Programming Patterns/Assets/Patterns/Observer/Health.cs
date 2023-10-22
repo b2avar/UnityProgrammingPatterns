13616d8204563d6099af083fde19506105208112
Burak Avar <burakavar@gmail.com>
2023-10-22 23:35:06 +0300

    unityevent added.

--- Unity Programming Patterns/Assets/Patterns/Observer/Health.cs
+++ Unity Programming Patterns/Assets/Patterns/Observer/Health.cs
@@ -2,32 +2,30 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class Health : MonoBehaviour {
-    [SerializeField] float fullHealth = 100f;
-    [SerializeField] float drainPerSecond = 2f;
-    float currentHealth = 0;
+namespace Patterns.Observer {
+    public class Health : MonoBehaviour {
+        [SerializeField] private float fullHealth = 100f;
+        [SerializeField] private float drainPerSecond = 2f;
+        private float _currentHealth = 0;
 
-    private void Awake() {
-        ResetHealth();
-        StartCoroutine(HealthDrain());
-    }
+        private void Awake() {
+            ResetHealth();
+            StartCoroutine(HealthDrain());
+        }
 
-    public float GetHealth()
-    {
-        return currentHealth;
-    }
+        public float GetHealth() {
+            return _currentHealth;
+        }
 
-    public void ResetHealth()
-    {
-        currentHealth = fullHealth;
-    }
+        public void ResetHealth() {
+            _currentHealth = fullHealth;
+        }
 
-    private IEnumerator HealthDrain()
-    {
-        while (currentHealth > 0)
-        {
-            currentHealth -= drainPerSecond;
-            yield return new WaitForSeconds(1);
+        private IEnumerator HealthDrain() {
+            while (_currentHealth > 0) {
+                _currentHealth -= drainPerSecond;
+                yield return new WaitForSeconds(1);
+            }
         }
     }
 }
--- Unity Programming Patterns/Assets/Patterns/Observer/Level.cs
+++ Unity Programming Patterns/Assets/Patterns/Observer/Level.cs
@@ -1,33 +1,34 @@
 using System.Collections;
+using Patterns.Observer;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Level : MonoBehaviour {
 
     [SerializeField] int pointsPerLevel = 200;
+    [SerializeField] private UnityEvent onLevelUp;
     int experiencePoints = 0;
 
-    IEnumerator Start()
-    {
-        while (true)
-        {
+    private IEnumerator Start() {
+        while (true) {
             yield return new WaitForSeconds(.2f);
             GainExperience(10);
         }
     }
 
-    public void GainExperience(int points)
-    {
+    private void GainExperience(int points) { 
+        var level = GetLevel();
         experiencePoints += points;
+        if (GetLevel() > level) {
+            onLevelUp.Invoke();
+        }
     }
 
-    public int GetExperience()
-    {
+    public int GetExperience() {
         return experiencePoints;
     }
 
-    public int GetLevel()
-    {
+    public int GetLevel() {
         return experiencePoints / pointsPerLevel;
     }
 }
